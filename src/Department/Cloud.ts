a60ba490dea701b59598d9d311774f6e45e9adb6
unifino <unifino@protonmail.com>
2022-02-24 08:33:19 +0300

    my app

--- src/Department/Cloud.ts
+++ src/Department/Cloud.ts
@@ -140,9 +140,9 @@ export function CloudOptimizer (): Promise<string[]> {
         CloudReport( [] )
         .then( cloud => {
 
-            let tmpRow = JSON.parse( JSON.stringify( cloud[0] ) );
-            cloud[0] = cloud[3];
-            cloud[3] = tmpRow;
+            // let tmpRow = JSON.parse( JSON.stringify( cloud[0] ) );
+            // cloud[0] = cloud[3];
+            // cloud[3] = tmpRow;
 
             for ( let row of cloud ) {
                 for ( let x of row.patch ) {
@@ -159,7 +159,7 @@ export function CloudOptimizer (): Promise<string[]> {
                     else newCloud.push( x );
                 }
             }
-            rs( [ newCloud2, newCloud.length ] as any );
+            rs( [ newCloud2, newCloud.length, cloud[0].patch.length ] as any );
         } )
         .catch ( err => rx( "EC08: " + err ) )
 
