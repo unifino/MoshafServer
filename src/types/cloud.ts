1cdc7c75093499a61f2db4a61d6ceef615bf0d45
unifino <unifino@protonmail.com>
2022-02-24 08:14:43 +0300

    my app

--- src/types/cloud.ts
+++ src/types/cloud.ts
@@ -8,11 +8,32 @@
 
 
 export interface Architecture {
-    id      : number    ,
-    patch   : string    ,
+    id      : number        ,
+    patch   : earthRaw[]    ,
 }
 
 export interface cloud_response {
     status: number,
     answer: Architecture[] | "string"
 }
+
+
+// -- =====================================================================================
+
+export type earthActions =
+      "BugReport"
+    | "Fav+"
+    | "Fav-"
+    | "Bound"
+    | "Unbound"
+    | "Comment"
+    // | "Uncomment"
+    ;
+
+export type Source = "Q" | "H" | "N" | "T" | "C";
+export type earthParcel = [ Source, number, string? ];
+export type earthValue = earthParcel | [ earthParcel, earthParcel ];
+
+export type earthRaw = [ earthActions, earthValue ];
+
+// -- =====================================================================================
